f398ab1043bc860ebc55efc9b87b32825d133f1f
schneidermobiledevelopment <schneider.mobiledevelopment@gmail.com>
2017-04-23 21:41:02 -0500

    HTTP Calls added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {HttpServices} from './todo.http.services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[HttpServices]
 })
-export class AppComponent {
-  title = 'TO DO APPLICATION';
+export class AppComponent implements OnInit {
+  title:string;
+  constructor(private _httpService:HttpServices){
+    this.title='';
+  }
+  ngOnInit(){
+    this.getTitle();
+  }
+  getTitle(){
+    this._httpService.getTodoData().subscribe(t=>this.title=t);
+  }
+
 }
---
+++ src/app/todo.http.services.ts
@@ -0,0 +1,35 @@
+import{Injectable} from '@angular/core'
+import { Http, Response }  from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+@Injectable()
+export class HttpServices{
+    data:string;
+    constructor(private http:Http){
+
+    }
+    getTodoData():Observable<string>{
+
+        return this.http.get("http://localhost:31193/api/Expense/GetExpense").map(this.extractData).catch(this.handleError);
+
+    }
+      private extractData(res: Response) {
+    //let body = res.json();
+    let body=res.text();
+    return body;
+  }
+    private handleError (error: Response | any) {
+    // In a real world app, you might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg); 
+  }
+}
